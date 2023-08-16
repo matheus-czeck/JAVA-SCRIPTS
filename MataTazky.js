d62f35d853775ae88f141f9585c94275f0e5f9ae
matheus-czeck <matheusczeckcz@gmail.com>
2023-08-16 17:55:17 -0300

    game aprt2

--- MataTazky.js
+++ MataTazky.js
@@ -1,4 +1,8 @@
-alert('teste')
-
-altura 
-largura
+var altura = 0 
+var largura = 0
+function ajustaTamanho() {
+ altura =  window.innerHeight
+  largura =  window.innerWidth
+  console.log(altura, largura)
+}
+ ajustaTamanho()
